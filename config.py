34a37ce8572d18b7471ca762a571e8b1212011a7
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-01 21:56:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/hkmusic/gojomusic49",
+    "https://github.com/HKMUSIC/AJAY-BOT-WIN",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", ""
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/II_CHATS_II")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/GOJO_SUPPORT_GROUP_II")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHAT", "https://t.me/updtes_group")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/updtes_group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
